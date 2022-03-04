deba0fc3a7396f8b2998ebb53d7d8b1dcf12b292
Benjamin Neeb <beneeb304@gmail.com>
2022-03-04 09:38:37 -0500

    Cleaned solution and removed unnecessary libraries

--- InvScanApp/frmMain.cs
+++ InvScanApp/frmMain.cs
@@ -1,14 +1,7 @@
-﻿using InvScanApp.Properties;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InvScanApp
--- InvScanApp/frmReports.cs
+++ InvScanApp/frmReports.cs
@@ -1,16 +1,12 @@
 ﻿using InvScanApp.Properties;
-using Microsoft.Reporting.WinForms;
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InvScanApp
